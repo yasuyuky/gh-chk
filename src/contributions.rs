03d5fd740a55e3c4938966f416f4695971985ebb
yasuyuky <yasuyuki.ymd@gmail.com>
2020-11-11 22:46:01 +0900

    Colorize output

--- src/contributions.rs
+++ src/contributions.rs
@@ -1,5 +1,7 @@
+use colored::Colorize;
 use serde::Deserialize;
 use serde_json::json;
+use std::collections::HashMap;
 
 #[derive(Deserialize)]
 struct Res {
@@ -43,10 +45,24 @@ pub async fn check(user: &str) -> surf::Result<()> {
     let q = json!({ "query": include_str!("query.contributions.graphql"), "variables": v });
     let res = crate::query::<Res>(&q).await?;
     let calendar = res.data.user.contributionsCollection.contributionCalendar;
+
+    let colormap: HashMap<&str, (u8, u8, u8)> = [
+        ("L1", (0x8C, 0xE7, 0x98)),
+        ("L2", (0x38, 0xBC, 0x51)),
+        ("L3", (0x29, 0x94, 0x3D)),
+        ("L4", (0x1B, 0x5D, 0x2B)),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
     for week in calendar.weeks {
         print!("{}: ", week.firstDay);
         for d in week.contributionDays {
-            print!("{:2} ", d.contributionCount)
+            let ck = d.color.get(31..33).unwrap_or_default();
+            let c = colormap.get(ck).unwrap_or(&(0xE6, 0xE8, 0xED));
+            let s = format!("{:3}", d.contributionCount);
+            print!("{} ", s.as_str().on_truecolor(c.0, c.1, c.2))
         }
         println!("");
     }

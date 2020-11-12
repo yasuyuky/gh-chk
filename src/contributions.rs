2ed829ac6eee23692f171953534e63e0eb834041
yasuyuky <yasuyuki.ymd@gmail.com>
2020-11-12 21:04:59 +0900

    Improve foreground colors

--- src/contributions.rs
+++ src/contributions.rs
@@ -46,11 +46,11 @@ pub async fn check(user: &str) -> surf::Result<()> {
     let res = crate::query::<Res>(&q).await?;
     let calendar = res.data.user.contributionsCollection.contributionCalendar;
 
-    let colormap: HashMap<&str, (u8, u8, u8)> = [
-        ("L1", (0x8C, 0xE7, 0x98)),
-        ("L2", (0x38, 0xBC, 0x51)),
-        ("L3", (0x29, 0x94, 0x3D)),
-        ("L4", (0x1B, 0x5D, 0x2B)),
+    let colormap: HashMap<&str, (&str, u8, u8, u8)> = [
+        ("L1", ("black", 0x8C, 0xE7, 0x98)),
+        ("L2", ("black", 0x38, 0xBC, 0x51)),
+        ("L3", ("white", 0x29, 0x94, 0x3D)),
+        ("L4", ("white", 0x1B, 0x5D, 0x2B)),
     ]
     .iter()
     .cloned()
@@ -60,9 +60,9 @@ pub async fn check(user: &str) -> surf::Result<()> {
         print!("{}: ", week.firstDay);
         for d in week.contributionDays {
             let ck = d.color.get(31..33).unwrap_or_default();
-            let c = colormap.get(ck).unwrap_or(&(0xE6, 0xE8, 0xED));
+            let c = colormap.get(ck).unwrap_or(&("black", 0xE6, 0xE8, 0xED));
             let s = format!("{:3}", d.contributionCount);
-            print!("{} ", s.as_str().on_truecolor(c.0, c.1, c.2))
+            print!("{} ", s.as_str().color(c.0).on_truecolor(c.1, c.2, c.3))
         }
         println!("");
     }

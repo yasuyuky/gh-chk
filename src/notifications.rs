702b37e345f04a904d60ded6e5c0339ee64f6190
yasuyuky <yasuyuki.ymd@gmail.com>
2020-11-16 22:30:23 +0900

    Add reason

--- src/notifications.rs
+++ src/notifications.rs
@@ -5,6 +5,7 @@ use serde::Deserialize;
 struct Notification {
     repository: Repository,
     subject: Subject,
+    reason: String,
 }
 #[derive(Deserialize)]
 struct Repository {
@@ -21,7 +22,8 @@ pub async fn check() -> surf::Result<()> {
     let res = crate::rest::get::<Notification>("notifications").await?;
     for n in res {
         println!(
-            "{} {} {}",
+            "{} {} {} {}",
+            n.reason.magenta(),
             n.repository.full_name,
             n.subject.ntype.cyan(),
             n.subject.title

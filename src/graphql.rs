33f2770966165a873a04dada539b38ce7cf43780
yasuyuky <yasuyuki.ymd@gmail.com>
2020-11-17 23:54:20 +0900

    Use const

--- src/graphql.rs
+++ src/graphql.rs
@@ -1,9 +1,10 @@
 use serde::de::DeserializeOwned;
 
+const URI: &str = "https://api.github.com/graphql";
+
 pub async fn query<T: DeserializeOwned>(q: &serde_json::Value) -> surf::Result<T> {
-    let uri = "https://api.github.com/graphql";
     let token = std::env::var("GITHUB_TOKEN")?;
-    let mut res = surf::post(&uri)
+    let mut res = surf::post(&URI)
         .header("Authorization", format!("bearer {}", token))
         .body(q.to_string())
         .await?;

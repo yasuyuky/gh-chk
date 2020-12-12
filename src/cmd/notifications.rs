0869e523f1e3e94be83772371ac5b573ba06807d
yasuyuky <yasuyuki.ymd@gmail.com>
2020-12-12 23:58:32 +0900

    Get issue and PR status

--- src/cmd/notifications.rs
+++ src/cmd/notifications.rs
@@ -1,5 +1,6 @@
 use colored::Colorize;
 use serde::Deserialize;
+use serde_json::json;
 
 #[derive(Deserialize)]
 struct Notification {
@@ -23,11 +24,13 @@ struct Subject {
 pub async fn list(page: usize) -> surf::Result<()> {
     let res = crate::rest::get::<Notification>("notifications", page).await?;
     for n in &res {
+        let status = get_status(&n.subject.url).await.unwrap_or_default();
         println!(
-            "{:10} {:10} {:11} {} {} {}",
+            "{:10} {:10} {:11} {:6} {} {} {}",
             n.id.black(),
             n.reason.magenta(),
             n.subject.ntype.yellow(),
+            status,
             n.repository.full_name.cyan(),
             n.subject.title,
             n.subject.url.green(),
@@ -54,3 +57,15 @@ enum Resource {
     PullRequestStatus { pr_state: String },
 }
 
+async fn get_status(api_url: &str) -> surf::Result<String> {
+    let url = api_url
+        .replace("api.github.com/repos", "github.com")
+        .replace("/pulls/", "/pull/");
+    let v = json!({ "url": url });
+    let q = json!({ "query": include_str!("../query/resource.status.graphql"), "variables": v });
+    let res = crate::graphql::query::<Res>(&q).await?;
+    Ok(match res.data.resource {
+        Resource::IssueStatus { issue_state } => issue_state,
+        Resource::PullRequestStatus { pr_state } => pr_state,
+    })
+}

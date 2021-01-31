68546b048abaa2afe6ca91506b77b17370d06574
yasuyuky <yasuyuki.ymd@gmail.com>
2021-01-31 23:45:38 +0900

    Improve variable names

--- src/cmd/contributions.rs
+++ src/cmd/contributions.rs
@@ -41,19 +41,19 @@ struct ContributionDay {
 
 pub async fn check(user: Option<String>) -> surf::Result<()> {
     let user = user.unwrap_or(crate::cmd::viewer::get().await?);
-    let v = json!({ "login": user });
-    let q = json!({ "query": include_str!("../query/contributions.graphql"), "variables": v });
+    let var = json!({ "login": user });
+    let q = json!({ "query": include_str!("../query/contributions.graphql"), "variables": var });
     let res = crate::graphql::query::<Res>(&q).await?;
     let calendar = res.data.user.contributions_collection.contribution_calendar;
 
     for week in calendar.weeks {
         print!("{}: ", week.first_day);
-        for d in week.contribution_days {
-            let r = u8::from_str_radix(d.color.get(1..3).unwrap_or_default(), 16)?;
-            let g = u8::from_str_radix(d.color.get(3..5).unwrap_or_default(), 16)?;
-            let b = u8::from_str_radix(d.color.get(5..7).unwrap_or_default(), 16)?;
-            let s = format!("{:3}", d.contribution_count);
-            print!("{} ", s.as_str().color("black").on_truecolor(r, g, b))
+        for day in week.contribution_days {
+            let r = u8::from_str_radix(day.color.get(1..3).unwrap_or_default(), 16)?;
+            let g = u8::from_str_radix(day.color.get(3..5).unwrap_or_default(), 16)?;
+            let b = u8::from_str_radix(day.color.get(5..7).unwrap_or_default(), 16)?;
+            let cnt = format!("{:3}", day.contribution_count);
+            print!("{} ", cnt.as_str().color("black").on_truecolor(r, g, b))
         }
         println!();
     }

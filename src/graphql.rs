2a43cb038b28456809e13aa76c58f49281db440e
yasuyuky <yasuyuki.ymd@gmail.com>
2021-11-12 09:05:39 +0900

    Add header to accept merge info

--- src/graphql.rs
+++ src/graphql.rs
@@ -6,6 +6,7 @@ const URI: &str = "https://api.github.com/graphql";
 pub async fn query<T: DeserializeOwned>(q: &serde_json::Value) -> surf::Result<T> {
     let mut res = surf::post(&URI)
         .header("Authorization", format!("bearer {}", TOKEN.to_owned()))
+        .header("Accept", "application/vnd.github.merge-info-preview+json")
         .body(q.to_string())
         .await?;
     Ok(res.body_json::<T>().await?)

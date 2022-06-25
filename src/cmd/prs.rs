50f6d4ce430e91a89e9de9ede83cecbf18f30957
yasuyuky <yasuyuki.ymd@gmail.com>
2022-06-25 22:19:35 +0900

    Use nestruct for prs

--- src/cmd/prs.rs
+++ src/cmd/prs.rs
@@ -3,58 +3,59 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::fmt::Display;
 
-#[derive(Serialize, Deserialize)]
-struct Res {
-    data: Data,
-}
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct Data {
-    repositoryOwner: RepositoriesOwner,
-}
-#[derive(Serialize, Deserialize)]
-struct RepositoriesOwner {
-    repositories: RepositoryConnection,
-}
-#[derive(Serialize, Deserialize)]
-struct RepositoryConnection {
-    nodes: Vec<Repository>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct SingleRepoRes {
-    data: SingleRepoData,
-}
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct SingleRepoData {
-    repositoryOwner: RepositoryOwner,
-}
-#[derive(Serialize, Deserialize)]
-struct RepositoryOwner {
-    repository: Repository,
+nestruct::nest! {
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    Repository {
+        name: String,
+        #[serde(rename="pullRequests")]
+        pull_requests: {
+            nodes: [{
+                number: usize,
+                title: String,
+                url: String,
+                #[serde(rename="mergeStateStatus")]
+                merge_state_status: #[serde(rename_all = "SCREAMING_SNAKE_CASE")] {
+                    Behind,
+                    Blocked,
+                    Clean,
+                    Dirty,
+                    Draft,
+                    HasHooks,
+                    Unknown,
+                    Unstable,
+                },
+            }]
+        }
+    }
 }
 
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct Repository {
-    name: String,
-    pullRequests: PullRequestsConnection,
-}
-#[derive(Serialize, Deserialize)]
-struct PullRequestsConnection {
-    nodes: Vec<PullRequest>,
-}
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct PullRequest {
-    pub number: usize,
-    pub title: String,
-    pub url: String,
-    pub merge_state_status: MergeStateStatus,
+nestruct::nest! {
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    Res {
+        data: {
+            repository_owner: {
+                repositories: {
+                    nodes: [ crate::cmd::prs::repository::Repository ]
+                }
+            }
+        }
+    }
 }
 
-impl Display for PullRequest {
+nestruct::nest! {
+    #[derive(serde::Serialize, serde::Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    RepoRes {
+        data: {
+            repository_owner: {
+                repository: crate::cmd::prs::repository::Repository
+            }
+        }
+    }
+}
+
+impl Display for repository::pull_requests::nodes::Nodes {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let s = format!(
             "{:>6} {} {} {}",
@@ -80,7 +81,7 @@ enum MergeStateStatus {
     Unstable,
 }
 
-impl MergeStateStatus {
+impl repository::pull_requests::nodes::merge_state_status::MergeStateStatus {
     fn to_emoji(&self) -> String {
         match self {
             Self::Behind => "⏩",
@@ -131,7 +132,7 @@ pub async fn check(slugs: Vec<String>) -> surf::Result<()> {
 async fn check_owner(owner: &str) -> surf::Result<()> {
     let v = json!({ "login": owner });
     let q = json!({ "query": include_str!("../query/prs.graphql"), "variables": v });
-    let res = crate::graphql::query::<Res>(&q).await?;
+    let res = crate::graphql::query::<res::Res>(&q).await?;
     match crate::config::FORMAT.get() {
         Some(&crate::config::Format::Json) => println!("{}", serde_json::to_string_pretty(&res)?),
         _ => print_owner_text(&res),
@@ -139,14 +140,14 @@ async fn check_owner(owner: &str) -> surf::Result<()> {
     Ok(())
 }
 
-fn print_owner_text(res: &Res) {
+fn print_owner_text(res: &res::Res) {
     let mut count = 0usize;
-    for repo in &res.data.repositoryOwner.repositories.nodes {
-        if repo.pullRequests.nodes.is_empty() {
+    for repo in &res.data.repository_owner.repositories.nodes {
+        if repo.pull_requests.nodes.is_empty() {
             continue;
         }
         println!("{}", repo.name.cyan());
-        for pr in &repo.pullRequests.nodes {
+        for pr in &repo.pull_requests.nodes {
             count += 1;
             println!("{}", pr);
         }
@@ -157,7 +158,7 @@ fn print_owner_text(res: &Res) {
 async fn check_repo(owner: &str, name: &str) -> surf::Result<()> {
     let v = json!({ "login": owner, "name": name });
     let q = json!({ "query": include_str!("../query/prs.repo.graphql"), "variables": v });
-    let res = crate::graphql::query::<SingleRepoRes>(&q).await?;
+    let res = crate::graphql::query::<repo_res::RepoRes>(&q).await?;
     match crate::config::FORMAT.get() {
         Some(&crate::config::Format::Json) => println!("{}", serde_json::to_string_pretty(&res)?),
         _ => print_repo_text(&res),
@@ -165,9 +166,9 @@ async fn check_repo(owner: &str, name: &str) -> surf::Result<()> {
     Ok(())
 }
 
-fn print_repo_text(res: &SingleRepoRes) {
+fn print_repo_text(res: &repo_res::RepoRes) {
     let mut count = 0usize;
-    for pr in &res.data.repositoryOwner.repository.pullRequests.nodes {
+    for pr in &res.data.repository_owner.repository.pull_requests.nodes {
         count += 1;
         println!("{}", pr);
     }

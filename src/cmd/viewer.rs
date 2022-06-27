1762398479ea5c3f064ff8df63dce17bac07d1ec
yasuyuky <yasuyuki.ymd@gmail.com>
2022-06-27 23:52:16 +0900

    Use nestruct for viewer

--- src/cmd/viewer.rs
+++ src/cmd/viewer.rs
@@ -1,21 +1,18 @@
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 
-#[derive(Serialize, Deserialize)]
-struct Res {
-    data: Data,
-}
-#[derive(Serialize, Deserialize)]
-struct Data {
-    viewer: Viewer,
-}
-#[derive(Serialize, Deserialize)]
-struct Viewer {
-    login: String,
+nestruct::nest! {
+    #[derive(serde::Deserialize, serde::Serialize)]
+    Res {
+        data: {
+            viewer: {
+                login: String
+            }
+        }
+    }
 }
 
 pub async fn get() -> surf::Result<String> {
     let q = json!({ "query": include_str!("../query/viewer.graphql") });
-    let res = crate::graphql::query::<Res>(&q).await?;
+    let res = crate::graphql::query::<res::Res>(&q).await?;
     Ok(res.data.viewer.login)
 }

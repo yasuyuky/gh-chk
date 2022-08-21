6b66ea109d0463d99319083531c4180254bdaef8
yasuyuky <yasuyuki.ymd@gmail.com>
2022-08-21 12:48:50 +0900

    Just dereference the lazy string

--- src/graphql.rs
+++ src/graphql.rs
@@ -5,7 +5,7 @@ const URI: &str = "https://api.github.com/graphql";
 
 pub async fn query<T: DeserializeOwned>(q: &serde_json::Value) -> surf::Result<T> {
     let mut res = surf::post(&URI)
-        .header("Authorization", format!("bearer {}", TOKEN.to_owned()))
+        .header("Authorization", format!("bearer {}", *TOKEN))
         .header("Accept", "application/vnd.github.merge-info-preview+json")
         .body(q.to_string())
         .await?;

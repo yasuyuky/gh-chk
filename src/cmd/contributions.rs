4e55ef257a1d249166e90d061c5636a5cce84354
yasuyuky <yasuyuki.ymd@gmail.com>
2024-05-19 21:59:48 +0900

    Update print_text function to display total contributions per week

--- src/cmd/contributions.rs
+++ src/cmd/contributions.rs
@@ -40,16 +40,17 @@ fn print_text(res: &res::Res) -> surf::Result<()> {
     let calendar = &res.data.user.contributions_collection.contribution_calendar;
     for week in &calendar.weeks {
         print!("{}: ", week.first_day);
-        let mut week_count = 0;
+        let mut week_count = 0f64;
         for day in &week.contribution_days {
-            week_count += day.contribution_count;
+            week_count += day.contribution_count as f64;
             let r = u8::from_str_radix(day.color.get(1..3).unwrap_or_default(), 16)?;
             let g = u8::from_str_radix(day.color.get(3..5).unwrap_or_default(), 16)?;
             let b = u8::from_str_radix(day.color.get(5..7).unwrap_or_default(), 16)?;
             let cnt = format!("{:3}", day.contribution_count);
             print!("{} ", cnt.as_str().color("black").on_truecolor(r, g, b))
         }
-        print!("{}", week_count);
+        let l = week.contribution_days.len() as f64;
+        print!("{} {:>5.2}", week_count, week_count / l);
         println!();
     }
     println!("total contributions: {}", calendar.total_contributions);

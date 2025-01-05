bde43d070142f156cce1b19a552e28f4b3a7122c
yasuyuky <yasuyuki.ymd@gmail.com>
2025-01-05 10:12:05 +0900

    Add year-to-date and month-to-date contribution counts in contribution display

--- src/cmd/contributions.rs
+++ src/cmd/contributions.rs
@@ -39,6 +39,13 @@ pub async fn check(user: Option<String>) -> surf::Result<()> {
 
 fn print_text(res: &res::Res) -> surf::Result<()> {
     let calendar = &res.data.user.contributions_collection.contribution_calendar;
+    let mut year_to_date = 0;
+    let mut month_to_date = 0;
+    let this_week = calendar.weeks.last().unwrap();
+    let today = this_week.contribution_days.last().unwrap().date.clone();
+    let today_year = today.chars().take(4).collect::<String>();
+    let today_month = today.chars().take(7).collect::<String>();
+
     for week in &calendar.weeks {
         print!("{}: ", week.first_day);
         let mut week_count = 0f64;
@@ -48,12 +55,20 @@ fn print_text(res: &res::Res) -> surf::Result<()> {
             let g = u8::from_str_radix(day.color.get(3..5).unwrap_or_default(), 16)?;
             let b = u8::from_str_radix(day.color.get(5..7).unwrap_or_default(), 16)?;
             let cnt = format!("{:3}", day.contribution_count);
-            print!("{} ", cnt.as_str().color("black").on_truecolor(r, g, b))
+            print!("{} ", cnt.as_str().color("black").on_truecolor(r, g, b));
+            if day.date.starts_with(&today_year) {
+                year_to_date += day.contribution_count;
+            }
+            if day.date.starts_with(&today_month) {
+                month_to_date += day.contribution_count;
+            }
         }
         let l = week.contribution_days.len() as f64;
         print!(" {:3} {:>5.2}", week_count, week_count / l);
         println!();
     }
     println!("total contributions: {}", calendar.total_contributions);
+    println!("year to date: {}", year_to_date);
+    println!("month to date: {}", month_to_date);
     Ok(())
 }

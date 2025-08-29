397875ef2245f89e3ae4f6829018abd9a1a28129
yasuyuky <yasuyuki.ymd@gmail.com>
2025-08-30 08:31:48 +0900

    fix(tui): specify lifetime for Text in make_diff_text function

--- src/cmd/tui.rs
+++ src/cmd/tui.rs
@@ -503,7 +503,7 @@ fn contrast_fg(r: u8, g: u8, b: u8) -> Color {
     }
 }
 
-fn make_diff_text(diff: &str) -> Text {
+fn make_diff_text(diff: &str) -> Text<'_> {
     let mut text = Text::default();
     for line in diff.lines() {
         let styled = if line.starts_with("===") {
